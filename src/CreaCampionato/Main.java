23baf9c36fcf56889dcf12df03ce900604582bfd
1Lg20 <lucagarnero6@gmail.com>
2025-08-29 16:58:33 +0200

    Primo commit

---
+++ src/CreaCampionato/Main.java
@@ -0,0 +1,10 @@
+package CreaCampionato;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Crea il tuo campionato");
+	}
+
+}
