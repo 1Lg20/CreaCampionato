745175e48060fd6b62217e4143ca197e15a95605
1Lg20 <lucagarnero6@gmail.com>
2025-08-31 17:37:34 +0200

    User Input Nome-Numero squadre

--- src/CreaCampionato/Main.java
+++ src/CreaCampionato/Main.java
@@ -1,6 +1,7 @@
 package CreaCampionato;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,22 +9,48 @@ public class Main {
 		// definisco l'array che contiente la lista di squadre
 		ArrayList<String> Squadre= new ArrayList<String>();
 		
+		//Array per i dati aggiuntivi
+		String[] Dati= new String[2];
+		
 		System.out.println("Crea il tuo campionato");
-		RichiediSquadre(Squadre);
+		System.out.println("\n<---------------------------------->\n");
+		
+		Richiedi(Squadre, Dati);
+		
+		System.out.println("\n<---------------------------------->\n");
+		
+		System.out.println("Nome campionato:"+Dati[0]);
+		
+		System.out.println("Numero squadre:"+Dati[1]);
+		
 		for (int i = 0; i < Squadre.size(); i++) {
 			System.out.println(Squadre.get(i));
-			
 		}
 	}
 	
 	/*
 	 * Metedo RichiediSquadre
-	 * 	riceve un arrayList
+	 * 	riceve un arrayList per contenere le square
+	 * 	riceve un array per i dati extra
+	 * 	chiede in input il nome del campionato
+	 * 	chiede in input il numero di squadre
 	 * 	carica all'interno dell'arrrayList i nomi delle squadre
-	 * 	Controlla che sia stato inserito un numero pari di sqiadre maggiore di 6
+	 * 	Controlla che sia stato inserito un numero pari di sqiadre maggiore di 5
 	 * */
-	private static void RichiediSquadre(ArrayList<String> squadre){
-		for (int i = 0; i < 10; i++) {
+	private static void Richiedi(ArrayList<String> squadre, String[] dati){
+		//definisco lo scanner per l'input dell'utente
+		Scanner myScanner =new Scanner(System.in);
+		int NumeroSquadre=1;
+		
+		System.out.println("Inserire il nome del campionato");
+		dati[0]=myScanner.nextLine();
+		while((NumeroSquadre % 2 != 0) || NumeroSquadre < 5) {
+			System.out.println("Inserire il numero di squadre");
+			NumeroSquadre= Integer.parseInt(myScanner.nextLine());
+		}
+		dati[1]= String.valueOf(NumeroSquadre);
+		
+		for (int i = 0; i < NumeroSquadre; i++) {
 			squadre.add("Squadra"+(i+1));
 		}
 	}
